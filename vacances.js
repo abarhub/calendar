f69e9b7fe3ea92a3a492ff839c53c7a84e7ee2a9
abarhub <abarhub@gmail.com>
2022-06-11 19:54:45 +0200

    ajout du calendrier et des places disponibles

---
+++ vacances.js
@@ -0,0 +1,12 @@
+var vacances =[
+{ "name":"Nathan LaFromboise","date":"2022-06-01"},
+{ "name":"Nathan LaFromboise","date":"2022-06-05"},
+{ "name":"Nathan LaFromboise","date":["2022-06-08","2022-06-10"]},
+{ "name":"Raoul LaRue","date":"2022-06-07"},
+];
+
+// jour: 1=lundi, 2=mardi, 3=mercredi, 4=jeudi, 5=vendredi
+var presence=[
+{"name":"Dupuis Jacques","jours":[1,2], "place":"A"},
+{"name":"Dupond Pierre","jours":[1], "place":"B"}
+];
