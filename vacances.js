708500573b24b314ea41c4093f0072278ceb430f
abarhub <abarhub@gmail.com>
2022-06-24 09:40:12 +0200

    amélioration de l'affichage du calendrier et des jours de présence

--- vacances.js
+++ vacances.js
@@ -1,12 +1,28 @@
-var vacances =[
-{ "name":"Nathan LaFromboise","date":"2022-06-01"},
-{ "name":"Nathan LaFromboise","date":"2022-06-05"},
-{ "name":"Nathan LaFromboise","date":["2022-06-08","2022-06-10"]},
-{ "name":"Raoul LaRue","date":"2022-06-07"},
-];
 
 // jour: 1=lundi, 2=mardi, 3=mercredi, 4=jeudi, 5=vendredi
-var presence=[
-{"name":"Dupuis Jacques","jours":[1,2], "place":"A"},
-{"name":"Dupond Pierre","jours":[1], "place":"B"}
-];
+
+var personnes=[
+    {
+        name:"Nathan LaFromboise",
+        vacances:[
+            "2022-06-01","2022-06-05",
+            ["2022-06-08","2022-06-10"]
+            ],
+        presence:[
+            {jour:[1,2], place:"A"},
+            {jour:[4], place:"B"}
+        ],
+        color: 'red'
+    },{
+        name:"Raoul LaRue",
+        vacances:[
+            "2022-06-07","2022-06-09",
+            ["2022-06-15","2022-06-20"]
+            ],
+        presence:[
+            {jour:[1], place:"B"},
+            {jour:[3], place:"A"}
+        ],
+        color: 'green'
+    }
+]
